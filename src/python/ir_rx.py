3b1b7e2e2b654c4a965b0549cd0d8c28d4ce3629
brianu <baurbina92@gmail.com>
2026-03-13 09:36:20 -0700

    restructure rc_car

---
+++ src/python/ir_rx.py
@@ -0,0 +1,45 @@
+class IR_Receiver():
+    def __init__(self, parent_class, pin = 3):
+        # IR receiver is on PMODB and connected to pin 3
+        self.enable = parent_class.weapons
+        self.logger = parent_class.logger
+        if not self.enable:
+            self.logger.info(f"IR receiver not initialized")
+            return
+
+        err = mb_pmodb.init_gpio(pin, GPIO_IN)
+        if (err != 0):
+            self.logger.error(
+                f"init_gpio failed for pin {pin}, err: {err}")
+        else:
+            self.logger.debug(
+                f"init_gpio successful for pin {pin}")
+
+        self.ir_pin = pin
+
+        # Start a process which indicates a hit
+        self.ir_rx_thread = threading.Thread(
+            target = self.notify_hit_t,
+            args = (parent_class, parent_class.pmodB_lock, ))
+
+        self.ir_rx_thread.start()
+
+        self.logger.info(f"Initialized IR Receiver thread")
+        self.logger.debug(f"IR receiver signal pin: {self.ir_pin}")
+
+    def notify_hit_t(self, car, lock):
+        if not self.enable:
+            return
+
+        lock.acquire()
+        prev_read = mb_pmodb.read_gpio(self.ir_pin)
+        lock.release()
+        while True:
+            lock.acquire()
+            curr_read = mb_pmodb.read_gpio(self.ir_pin)
+            lock.release()
+            if curr_read == 0 and curr_read != prev_read:
+                self.logger.info("Hit!")
+                car.stop()
+            prev_read = curr_read
+            time.sleep(0.01)
---
+++ src/python/ir_tx.py
@@ -0,0 +1,79 @@
+class IR_Transmitter:
+    def __init__(self, parent_class, pin = 7):
+        # IR transmitter "laser" is on PMODB and connected to
+        # pin 7
+
+        self.logger = parent_class.logger
+        self.enable = parent_class.weapons
+        if not self.enable:
+            self.logger.info(f"Laser not initialized")
+            return
+
+        err = mb_pmodb.init_pwm(pin)
+        if (err != 0):
+            self.logger.error(
+                f"init_pwm failed for pin {pin}, err: {err}")
+        else:
+            self.logger.debug(
+                f"init_pwm successful for pin {pin}")
+
+        self.pwm_period_usec = 26 # 26 usec is about 38.4 KHz
+        self.pwm_duty_cycle = 50
+        self.pwm_pin = pin
+        self.laser_pulse_duration_msec = 250 # 250 msec pulse
+
+        self.shoot_event = threading.Event()
+        self.shoot_thread = threading.Thread(
+            target = self.shoot_t,
+            args = (parent_class.pmodB_lock, self.shoot_event))
+
+        self.shoot_thread.start()
+
+        self.logger.info(f"Initialized laser shoot thread")
+        self.logger.debug(f"Laser signal pin: {self.pwm_pin}")
+        self.logger.debug(
+            f"Laser pulse duration: {self.laser_pulse_duration_msec} milliseconds")
+
+    def shoot_t(self, lock, shoot_event):
+        while True:
+            # Wait until shoot_event is set
+            if not shoot_event.is_set():
+                continue
+
+            self._shoot(lock)
+            shoot_event.clear()
+
+    def _shoot(self, lock):
+        if not self.enable:
+            self.logger.debug(f"Laser could not be shot - not initialized!")
+            return
+
+        # Shoot a quarter second pulse "laser"
+        lock.acquire()
+        err = mb_pmodb.start_pwm(
+            self.pwm_pin,
+            self.pwm_period_usec,
+            self.pwm_duty_cycle)
+        lock.release()
+
+        if (err != 0):
+            self.logger.error(
+                f"start_pwm failed for pin {self.pwm_pin}, err: {err}")
+        else:
+            self.logger.debug(
+                f"start_pwm successful for pin {self.pwm_pin}")
+
+        time.sleep(self.laser_pulse_duration_msec / 1000)
+
+        lock.acquire()
+        err = mb_pmodb.stop_pwm(self.pwm_pin)
+        lock.release()
+
+        if (err != 0):
+            self.logger.error(
+                f"stop_pwm failed for pin {self.pwm_pin}, err: {err}")
+        else:
+            self.logger.debug(
+                f"stop_pwm successful for pin {self.pwm_pin}")
+
+        self.logger.info(f"Laser shot!")
